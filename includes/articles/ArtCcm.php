9b4bd22864d7ca3452947a8d04583813d827ef1c
Thomas Bévière <tom.beviere@gmail.com>
2016-12-05 20:53:56 +0100

    Intégration de la F.A.Q dans la page ccm

--- includes/articles/ArtCcm.php
+++ includes/articles/ArtCcm.php
@@ -9,4 +9,219 @@
 
         </div>
     </div>
+
+    <h3><span class="label label-primary">F.A.Q</span></h3>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h2 class="panel-title">Stage</h2>
+        </div>
+        <div class="panel-body">
+            <h4>Qu'est ce qu'un stage ?</h4>
+
+            Le stage est une période temporaire "de mise en situation en milieu professionnel" au cours de laquelle l'étudiant acquiert des compétences professionnelles tout en métant ses prorpes compétences au service de l'entreprise qui l'accueille pour lui permettre de se développer par le biais de l'acomplissement de missions.
+            <br />
+            Le but du stage est  d'obtenir un diplôme ou une certification en vue de favoriser l'insertion professionnelle de l'étudiant.
+
+        </div>
+
+        <div class="panel-body">
+            <h4>Qu'est ce qu'un stagiaire?</h4>
+
+            Le stagiaire ne peut être considéré comme un salarié.
+            Ainsi, il ne peut avoir comme mission l'exécution d'une tâche régulière
+            correspondant à un poste de travail permanent de l'entreprise.
+            Son stage a uniquement pour but une mise en situation en milieu professionnel afin d'acquérir des compétences professionnelles et mettre en oeuvre les acquis de sa formation.
+            Le stagiaire est un étudiant.
+        </div>
+
+        <div class="panel-body">
+            <h4>L'entreprise peut-elle forcer un étudiant à être présent la nuit ? Le dimanche ? Un jour férié ?</h4>
+
+            Oui, à condition que cela soit en lien avec le sujet de stage et que cela soit prévu dans la convention de stage. Dans ce cas l'étudiant sera gratifié pendant cette période si le stage est supérieur à 308h.
+        </div>
+    </div>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Étudiant</h3>
+        </div>
+        <div class="panel-body">
+            <h4>L'entreprise peut-elle déduire de la rémunération la prise en charge de frais de restauration, de transport ou autres avantages ?</h4>
+
+            Non. La gratification est due sans préjudice du remboursement des frais ou avantages offerts.
+            <br />
+            Référence : art. D124-8 du code de l'éducation.
+        </div>
+
+        <div class="panel-body">
+            <h4>La rémunération obtenue est-elle exonérée d'impôt sur le revenu ?</h4>
+
+            Oui, dans la limite du montant annuel du salaire minimum de croissance, que le stagiaire soit personnellement imposable ou non.
+            <br />
+            Référence : art. 81 bis du code général des impôts.
+        </div>
+
+        <div class="panel-body">
+            <h4>Que risque le stagiaire en cas de manquement ou de faute disciplinaire au sein de l'enterprise ?</h4>
+
+            L’organisme d’accueil en informe immédiatement l’établissement d’enseignement supérieur, qui seul pourra saisir la section disciplinaire.
+            <br />
+            En effet, le stagiaire conservant son statut d’étudiant et en l’absence de lien hiérarchique avec le tuteur de stage, l’organisme d’accueil ne peut décider de lui infliger une sanction disciplinaire. Par contre, en cas de manquement particulièrement grave à la discipline, l’organisme d’accueil pourra mettre fin au stage. Dans l’hypothèse où l’organisme d’accueil décide d’interrompre le stage, l’établissement d’enseignement supérieur pourra décider de la validation – ou non - du stage, même s’il n’a pas atteint la durée prévue dans le cursus.
+            <br />
+            Références :
+            <br />- art L124-15 du code de l’éducation : « En cas de rupture de la convention à l'initiative de l'organisme d'accueil, l'autorité académique ou l'établissement d'enseignement supérieur valide la période de formation en milieu professionnel ou le stage, même s'il n'a pas atteint la durée prévue dans le cursus, ou propose au stagiaire une modalité alternative de validation de sa formation. En cas d'accord des parties à la convention, un report de la fin de la période de formation en milieu professionnel ou du stage, en tout ou partie, est également possible. »
+            <br />- art R712-9 et suivants du code de l’éducation.
+        </div>
+
+        <div class="panel-body">
+            <h4>Est-il possible de mettre fin au stage avant la date prévue ?</h4>
+
+            Oui. L'étudiant doit en informer son enseignant référent et son tuteur de stage afin de leur expliquer le motif.
+            <br />
+            Un avenant à la convention de stage devra ensuite être signé.
+            <br />
+            Référence :<br />
+            art. L124-15 et D124-4 du code de l’éducation « Lorsque le stagiaire interrompt sa période de formation en milieu professionnel ou son stage pour un motif lié à la maladie, à un accident, à la grossesse, à la paternité, à l'adoption ou, en accord avec l'établissement, en cas de non-respect des stipulations pédagogiques de la convention ou en cas de rupture de la convention à l'initiative de l'organisme d'accueil, l'autorité académique ou l'établissement d'enseignement supérieur valide le stage, même s'il n'a pas atteint la durée prévue dans le cursus, ou propose au stagiaire une modalité alternative de validation de sa formation. En cas d'accord des parties à la convention, un report de la fin de la période de formation en milieu professionnel ou du stage, en tout ou partie, est également possible. »
+        </div>
+
+        <div class="panel-body">
+            <h4>Est-il possible de bénéficier de congés ?</h4>
+            Le stagiaire n'ayant le statut d'un salarié, l’employeur peut refuser de lui accorder des congés. Par ailleurs les périodes d’absence doivent être explicitement rédigées dans la convention de stage.
+
+        </div>
+
+    </div>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Tuteur de stage</h3>
+        </div>
+        <div class="panel-body">
+            <h4>Comment est choisi le tuteur de stage ?</h4>
+
+            Le tuteur de stage est désigné par l'entreprise qui va accueillir l'étudiant. Attention, un tuteur de stage ne peut encadrer qu'un nombre maximum de stagiaires durant la semaine civile en cours.
+        </div>
+
+        <div class="panel-body">
+            <h4>Quel est le rôle du tuteur de stage ?</h4>
+
+            Le tuteur est chargé de l'accueil, de l'accompagnement du stagiaire et du respect des clauses mentionnées dans la convention de stage. Les tâches confiées au tuteur peuvent être prévues dasn le cadre d'un accord d'entreprise, de même que les conditions de l'éventuelle valorisation de cette fonction. Le tuteur est en lien régulier avec l'enseignant-référent. Il doit l'alerter sur les difficultés pouvant intervenir durant le stage et redéfinir avec l'enseignant-référent les missions confiées au stagiaire.
+        </div>
+    </div>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Professeur référent</h3>
+        </div>
+        <div class="panel-body">
+            <h4>Comment est choisi l'enseignant référent ?</h4>
+
+            L'enseignant référent doit être choisi parmi l'équipe pédagogique. Il s'agit obligatoirement d'un enseignant ou enseignant-chercheur, titulaire ou contractuel. Il ne peut en aucun cas s'agir d'un personnel administratif.
+            <br />
+            Il doit avoir un lien avec le sujet du stage afin de pouvoir définir au mieux les compétences à acquérir ou à développer au cours du stage.
+        </div>
+
+        <div class="panel-body">
+            <h4>Quel est le rôle de l'enseignant référent ?</h4>
+
+            L'enseignant référent doit s'assurer à plusieurs reprises du bon déroulement du stage auprès du tuteur de stage et du respect des clauses de la convention de stage. Il est responsable du suivi pédagogique du stage. Il peut éventuellement proposer une redéfinition des missions du stage.
+            <br />
+            Le conseil d'administration de l'établissement d'enseignement supérieur détermine les modalités du suivi régulier.
+        </div>
+
+    </div>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Rapport de stage</h3>
+        </div>
+        <div class="panel-body">
+            <h4>Comment rédiger un rapport de stage ?</h4>
+
+            Votre rapport doit contenir les éléments suivants :<br />
+
+            Page de garde : vos nom et prénom ; intitulé du poste (et type de contrat) ; dates du stage ; nom de l'entreprise avec éventuellement son logo ; coordonnées de votre lieu de formation et noms de votre maître de stage et/ou membres de votre jury.
+            <br />
+            Plan détaillé : Le plan peut être en deux ou trois parties avec une introduction et une conclusion. Chaque partie contient plusieurs paragraphes numérotés (1 > 1.1 > 1.2, etc.)
+            <br />
+            Vous êtes nombreux à vous demander "Comment faire un rapport de stage ?" Voici donc les questions importantes à mettre en avant pour la rédaction de votre rapport de stage :
+            <br />
+            <ul>
+                <li>Qu'avez vous tiré de votre stage ?</li>
+                <li>Concrètement, qu'avez vous apporté à l'entreprise ?</li>
+                <li>Quelles connaissances avez vous acquises ?</li>
+                <li>Avez-vous atteint les objectifs que vous vous étiez donné au début du stage ?</li>
+                <li>Avez-vous bien réussi à gérer votre temps ?</li>
+                <li>Quels outils maîtrisez-vous plus qu'au début de votre stage ?</li>
+                <li>Souhaiteriez-vous à nouveau travailler dans ce genre d'entreprise ? Justifiez</li>
+                <li>Ce stage vous a-t-il aidé à choisir une orientation professionnelle pour votre avenir ?</li>
+            </ul>
+
+        </div>
+
+        <div class="panel-body">
+            <h4>Comment préparer mon rapport de stage ?</h4>
+
+            Il vous est conseillé de prendre des notes au cours de votre stage et de tenir un journal de bord.
+            <br />
+            Cela vous permettra par la suite de faire le point sur le contenu du stage, sur ce qu’il vous a apporté et comment il a été en lien avec votre projet professionnel.
+
+        </div>
+
+        <div class="panel-body">
+            <h4>Problématique de rapport de stage : c'est quoi ?</h4>
+
+            Une problématique est une présentation d'un problème sous différents aspects.
+            <br />
+            La problématique guide la réflexion sur le sujet et ouvre des axes de recherche qui permettent de préciser les différents arguments qui alimenteront le développement du rapport de stage.
+
+        </div>
+
+        <div class="panel-body">
+            <h4>Problématique de rapport de stage : comment la formuler ?</h4>
+
+            Pour formuler votre problématique, transformez vos actions et vos tâches dans l'entreprise en questions. Après analyse, vous vous apercevrez que chacune d'entre elles répond à une problématique particulière.
+            <br />
+            Vous pouvez alors transformer en une phrase l'ensemble des problèmes ou besoins rencontrés par une entreprise et pour lesquels elle vous a missionné afin d'y répondre. Votre problématique de rapport de stage est alors prête.
+
+        </div>
+
+        <div class="panel-body">
+            <h4>Remerciements dans le rapport de stage, comment faire ?</h4>
+
+            Exprimer sa gratitude en début de rapport de stage dans une partie dédiée peut paraître inutile, ou obsolète, mais elle est pourtant fondamentale. Il ne s'agit pas d'un exercice de lèche-bottes, mais d'un renvoi d'ascenseur à l'entreprise qui vous a accepté pendant plusieurs mois et au maître de stage qui vous a supervisé.
+            <br />
+            La rubrique remerciement du rapport de stage peut être couplée à l'introduction du rapport de stage ou être indépendante. Si vous choisissez la première option, essayez de trouver une astuce de mise en page qui montre que vous n'avez pas oublié la rubrique. Si vous avez choisi la deuxième option, gardez en tête que vous n'êtes pas obligé de remplir une page entière pour vos remerciements mais placez bien cette partie sur une page à part de l'introdction.
+
+            Les axes à développer pour vos remerciements dans le rapport de stage peuvent être :<br />
+            <ul>
+                <li>le fait d'avoir bénéficié de la confiance de l'employeur et/ou du maître de stage</li>
+                <li>l'accueil de l'équipe de l'entreprise</li>
+                <li>l'intérêt que vous avez porté à votre travail</li>
+                <li>l'équipe de votre école (si elle a joué un rôle de suivi durant votre stage)</li>
+            </ul>
+            La rubrique remerciements du rapport de stage s'inscrit dans le trio Introduction / Remerciements / Problématique. Il faut que ces trois éléments soient liés et s'enchaînent de manière cohérente.
+
+        </div>
+
+    </div>
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Sources</h3>
+        </div>
+        <div class="panel-body">
+            <ul>
+                <li><a href="http://affaires-juridiques.upv.univ-montp3.fr/files/2015/02/FAQ_StagesUPV_v11.pdf">affaires-juridiques.upv.univ-montp3.fr</a></li>
+                <li><a href="http://www.u-pec.fr/etudiant/stages-emplois/stages-foire-aux-questions-faq--556991.kjsp">u-pec.fr­</a></li>
+                <li><a href="http://www.commentcamarche.net/faq/16226-comment-faire-un-rapport-de-stage">commentcamarche.net</a></li>
+                <li><a href="https://www.digischool.fr/rapport-de-stage/">digischool.fr</a></li>
+            <ul>
+
+        </div>
+    </div>
+
+
+
 </div>
